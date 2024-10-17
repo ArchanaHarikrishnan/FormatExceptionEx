a93b70801565db9d7107541ca0003c49d9632193
ArchanaHarikrishnan <archana18harikrishnan@gmail.com>
2024-10-17 13:17:12 +0530

    Add project files.

---
+++ FormatExceptionEx/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FormatExceptionEx
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string Name = Console.ReadLine();
+
+            try
+            {
+                int number = int.Parse(Name);
+                Console.WriteLine("You entered the number: " + number);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
